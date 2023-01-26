9ccef4bdb37809c61da199b42080628d452e9674
Sebastian Post <sebastianpost@protonmail.com>
2023-01-26 16:04:23 +0100

    Render products from JS function

---
+++ drinksArray.js
@@ -0,0 +1,51 @@
+export const categories = [
+    {
+        name: "Nocco Focus",
+        desc: "Drycken innehåller koffein och vitaminer. De är kolsyrade och helt fria från socker."
+    },
+    {
+        name: "Nocco Koffeinfri",
+        desc: "Drycken innehåller BCAA och är helt fri från koffein. De är icke kolsyrade och fria från socker."
+    },
+    {
+        name: "Nocco PWO",
+        desc: "Drycken är berikad med BCAA, koffein, sju vitaminer och är helt utan socker."
+    },
+    {
+        name: "Nocco BCAA",
+        desc: "Drycken innehåller BCAA och aminosyror, koffein, vitaminer samt även extrakt av grön te. De är kolsyrade och helt fria från socker."
+    }
+
+]
+
+export const noccoCanArray = [
+    {
+        id: 0,
+        category: ["Focus", "all"],
+        img: ["/assets/Drinks/RaspberryFocus.png", "/assets/Drinks/Cola.png"],
+        alt: ["Black Nocco can with raspberry blast flavour", "Black nocco can with Cola flavour"],
+        categoryDesc: categories[0].desc,
+    },
+    {
+        id: 1,
+        category: ["Caffeine Free", "all"],
+        img: ["/assets/Drinks/carribien.png", "/assets/Drinks/äpple.png"],
+        alt: ["Green nocco can with carribean/pineapple flavour", "Green nocco can with apple flavour"],
+        categoryDesc: categories[1].desc,
+    },
+    {
+        id: 2,
+        category: ["PWO", "all"],
+        img: ["/assets/Drinks/cherry.png","/assets/Drinks/blueRaspberry.png"],
+        alt: ["Silver nocco can with cherry flavour", "Silver nocco can with blue raspberry flavour"],
+        categoryDesc: categories[2].desc,
+    },
+    {
+        name: "Breeze",
+        id: 6,
+        category: ["BCAA", "all"],
+        img: ["/assets/Drinks/breez.png","/assets/Drinks/tomten.png"],
+        alt: ["Lightblue nocco can with juicy breeze flavour","Pink nocco can with a picture of santa and with skumtomte flavour"],
+        categoryDesc: categories[3].desc,
+    },
+]
--- main.js
+++ main.js
@@ -1,4 +1,33 @@
 import './style.css'
+import { noccoCanArray, categories } from './drinksArray.js'
+
+
+const menuBtn = document.getElementById('menuBtn');
+const closeBtn = document.getElementById('closeBtn');
+const cookiesBtn = document.getElementById("cookiesBtn");
+menuBtn.addEventListener("click", openCloseMenu);
+closeBtn.addEventListener("click", openCloseMenu);
+cookiesBtn.addEventListener("click", closeCookies);
+document.querySelector(".noccoLogo").classList.remove("hidden");
+document.querySelector(".logo").classList.remove("hidden");
+document.querySelector(".cookies").classList.remove("hidden");
+const cookies = document.querySelector(".cookies")
+
+function renderProducts() {
+for (let j = 0; j < noccoCanArray.length; j++) {
+    document.querySelector('#assortment').innerHTML += `
+		<h1>${noccoCanArray[j].category[0]}
+        <h2>${noccoCanArray[j].categoryDesc}</h2>
+          <div class="img">
+			<img id="imageOne-${j}" src="${noccoCanArray[j].img[0]}" width="75" height="200" loading="lazy" alt="${noccoCanArray[j].alt[0]}"/>
+			<img id="imageTwo-${j}" src="${noccoCanArray[j].img[1]}" width="75" height="200" loading="lazy" alt="${noccoCanArray[j].alt[1]}"/>		
+			</div>
+		`
+	}
+}
+
+
+
 
 
 document.querySelector('#slideShow').innerHTML = `
@@ -12,20 +41,13 @@ arrow_right
 </button>
 </div>
 `
-const menuBtn = document.getElementById('menuBtn');
-const closeBtn = document.getElementById('closeBtn');
+
+
+
 const arrowLeft = document.getElementById('arrow_left');
 const arrowRight = document.getElementById('arrow_right');
-const cookiesBtn = document.getElementById("cookiesBtn");
-menuBtn.addEventListener("click", openCloseMenu);
-closeBtn.addEventListener("click", openCloseMenu);
 arrowLeft.addEventListener("click", imageSliderMinus);
 arrowRight.addEventListener("click", imageSliderPlus);
-cookiesBtn.addEventListener("click", closeCookies);
-document.querySelector(".noccoLogo").classList.remove("hidden");
-document.querySelector(".logo").classList.remove("hidden");
-document.querySelector(".cookies").classList.remove("hidden");
-const cookies = document.querySelector(".cookies")
 
 let i = 0;
 let images = [];
@@ -54,7 +76,7 @@ function imageSliderMinus(){
 imageSliderPlus()
 
 function openCloseMenu(){
-console.log('hej');
+console.log('Stäng och öppna menyn');
 
 	menuBtn.classList.toggle('hidden');
 	closeBtn.classList.toggle('show');
@@ -62,14 +84,18 @@ console.log('hej');
 	hamburger.style.zIndex="100"
 }
 function closeCookies(){
-	console.log('hej');
+	console.log('Stänga cookiebaren');
 	cookies.classList.add('hidden');
 
   }
-
+  renderProducts();
   /* SLASK
 
-
+document.querySelector('#sectionOne').innerHTML = `
+<h3>${categories[0].name}</h3>
+<p>${categories[0].desc}</p>
+<img src="${noccoCanArray[0].img}" width="75" height="200" loading="lazy" alt="${noccoCanArray[0].img.alt}"/>
+<img src="${noccoCanArray[1].img}" width="75" height="200" loading="lazy" alt="${noccoCanArray[1].img.alt}" />
 
   	cookiesBtn.classList.toggle('show');
 	hamburger.classList.toggle('hidden');
